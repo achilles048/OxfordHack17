f115293c5e44c993ba95302b09fc7ae96d4acfce
achilles048 <achilles048@gmail.com>
2017-11-26 10:33:11 +0000

    Add files via upload
    Chooses the papers to hand to the researchers.

---
+++ relevantpapers.py
@@ -0,0 +1,28 @@
+import xml.etree.ElementTree as ET
+import requests
+import database
+
+def findrelevantpapers(disease):
+    relevant_papers = []
+    x = requests.get('https://wsearch.nlm.nih.gov/ws/query?db=healthTopics&term=' + disease + '&retmax=50')
+    with open('test.xml','wb' ) as file:
+        file.write(x.content)
+    tree = ET.parse('test.xml')
+    root = tree.getroot()
+    for line in root.iter('count'):
+        no_relevant_papers = (line.text)
+    for line in root.iter('document'):
+        relevant_papers.append(line.attrib['url'])
+
+    print(relevant_papers)
+    return (relevant_papers)
+
+def updatepapers(disease):
+    original_papers = database.findDisease(disease)
+    current_papers = findrelevantpapers(disease)
+    for paper in current_papers:
+        if not database.findPaper(disease,paper):
+            database.addPaper(disease,paper)
+
+
+
